237b5475c5d890ca5cca3a7d85f2c324805b0e3f
vairakkumar vairaperumal <vvairaperumal@deloitte.com>
2018-12-12 22:33:00 -0800

    updated to use strict

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const double = (n) => n * 2;
 const increment = (n) => n + 1;
+'use strict';
+
 const increment3 = (n) => n + 3;
 console.log('using node 10.x, @babel/core@7.x');
 console.log('npm i -D @babel/cli @babel/core @babel/present-env');
--- output.js
+++ output.js
@@ -4,6 +4,8 @@ const double = n => n * 2;
 
 const increment = n => n + 1;
 
+'use strict';
+
 const increment3 = n => n + 3;
 
 console.log('using node 10.x, @babel/core@7.x');
